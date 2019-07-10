f1d5db905a7a4a01644faef89aec3c04b61cc2bf
Marcin <niemir69@gmail.com>
2019-07-10 21:32:28 +0200

    Updates

---
+++ precache-manifest.47ad4fa9205026e5c28ad2f79ef1ded7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8888be0de3513bb1d3185b047f7ced33",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "47042676ec3f7db1bf63",
+    "url": "/WeatherApp/static/css/main.6c15f03d.chunk.css"
+  },
+  {
+    "revision": "d2103c5e095edc1346e1",
+    "url": "/WeatherApp/static/js/2.a68ccf2f.chunk.js"
+  },
+  {
+    "revision": "47042676ec3f7db1bf63",
+    "url": "/WeatherApp/static/js/main.2e51d264.chunk.js"
+  },
+  {
+    "revision": "4efbdecc212de1558e2a",
+    "url": "/WeatherApp/static/js/runtime~main.3044ef82.js"
+  },
+  {
+    "revision": "b168e3b6f58f1631379ada753cc66d17",
+    "url": "/WeatherApp/static/media/cloudy.b168e3b6.png"
+  },
+  {
+    "revision": "b2ebe4cee477c2ac547b08fa726af447",
+    "url": "/WeatherApp/static/media/rain2.b2ebe4ce.png"
+  },
+  {
+    "revision": "d94d0f8773572363e5228a5731229ff8",
+    "url": "/WeatherApp/static/media/sunny.d94d0f87.png"
+  },
+  {
+    "revision": "ddc22aa99b064dc3d1c08321a435a51d",
+    "url": "/WeatherApp/static/media/walking-large.ddc22aa9.jpg"
+  }
+]);
